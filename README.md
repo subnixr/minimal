9d93a87d616e10f617da7d8221394d78c7247237
subnixr <subnixr@gmx.com>
2016-09-11 20:53:33 +0200

    ripped from my dotfiles

---
+++ README.md
@@ -0,0 +1,81 @@
+# MINIMAL
+A minimal zsh theme.
+
+# Screenshot
+![alt tag](http://i.imgur.com/gs1e2rc.png)
+
+# Installation
+[Zgen](https://github.com/tarjoilija/zgen):
+```
+zgen load subnixr/minimal minimal
+```
+
+[Antigen](https://github.com/zsh-users/antigen):
+```
+antigen theme subnixr/minimal minimal
+```
+
+Otherwise, you can always clone the repo and source `minimal.zsh`.
+
+# Configuration
+This theme can be customized before sourcing through variables:
+
+- `MINIMAL_PROMPT`: enables prompt customization (default: active)
+- `MINIMAL_RPROMPT`: enables right prompt (default: active)
+- `MINIMAL_MAGIC_ENTER`: enables magic enter (default: active)
+- `MINIMAL_USER_CHAR`: overrides normal user character (default: `λ`)
+- `MINIMAL_INSERT_CHAR`: overrides insert mode character (default: `›`)
+- `MINIMAL_NORMAL_CHAR`: overrrides normal mode character (default: `·`)
+
+To disable a feature, just set it to something other than `yes` before  sourcing.
+
+Example:
+```
+MINIMAL_MAGIC_ENTER="I Don't want this"
+source /path/to/minimal.zsh
+```
+
+# Description
+A prompt with all features:
+```
+asd λ ›                                                 Workspace/sample master
+```
+On the left:
+
+- `asd` is the `basename` of python's virtualenv currently in use. It won't be 
+shown if not in use.
+- `λ` is shown if you are a normal user. When root, a classic `#` will be shown
+instead.
+- `λ` will be green if the last command exited successfully, otherwise will be 
+red.
+- `λ` will be underlined if you have jobs in background.
+- `›` will be show if you are in insert (default) mode. If using vimode, `·` 
+is shown when in normal mode.
+
+On the right:
+
+- The last two segments of `pwd` are shown. If a segment is longer than 10 chars, it will be ellipsed. If you are near the root (eg: `/usr/bin`) the first slash will be shown.
+- Git's current branch. It will be shown only if inside a git repo. It will be green if clean, otherwise red.
+
+# Magic Enter
+You may miss some info from your prompt: magic enter to the rescue!
+```
+λ › [subnixr@lambda-arch:~] [14 (72)] [1&]                                    ~
+Desktop    Downloads  GBA    pgadmin.log  Pictures  Templates  VirtualBox VMs
+Documents  Dropbox    Music  Phone        Public    Videos     Workspace
+```
+If your buffer is empty, pressing enter will print some useful informations:
+
+- Username, hostname and full `pwd`.
+- Number of visible files, number of total files.
+- Number of background jobs (if any).
+- Colored `ls` output, wrapped in `$PAGER` if output is longer than your view.
+
+#  I HATE UTF-8!
+No problem:
+```
+MINIMAL_USER_CHAR="$"
+MINIMAL_INSERT_CHAR=">"
+MINIMAL_NORMAL_CHAR="-"
+source /path/to/minimal.zsh
+```
---
+++ minimal.zsh
@@ -0,0 +1,135 @@
+# enable/disable switches
+MINIMAL_PROMPT="${MINIMAL_PROMPT:-yes}"
+MINIMAL_RPROMPT="${MINIMAL_RPROMPT:-yes}"
+MINIMAL_MAGIC_ENTER="${MINIMAL_MAGIC_ENTER:-yes}"
+
+# customization parameters
+MINIMAL_USER_CHAR="${MINIMAL_USER_CHAR:-λ}"
+MINIMAL_INSERT_CHAR="${MINIMAL_INSERT_CHAR:-›}"
+MINIMAL_NORMAL_CHAR="${MINIMAL_NORMAL_CHAR:-·}"
+
+# necessary
+autoload -U colors && colors
+setopt prompt_subst
+
+_grey="[38;5;244m"
+_greyp="%{$_grey%}"
+
+function minimal_git {
+  local statc="%{[0;32m%}" # assumes is clean
+  local bname="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+
+  if [ -n "$bname" ]; then
+    [ -n "$(git status --porcelain 2> /dev/null)" ] && statc="%{[0;31m%}"
+    echo " $statc$bname%{[0m%}"
+  fi
+}
+
+function minimal_path {
+  local w="%{[0m%}"
+  local cwd="%2~"
+  local pi=""
+  cwd="${(%)cwd}"
+  cwd=("${(@s:/:)cwd}")
+
+  for i in {1..${#cwd}}; do
+    pi="$cwd[$i]"
+    [ "${#pi}" -gt 10 ] && cwd[$i]="${pi:0:4}$w..$_greyp${pi: -4}"
+  done
+
+  echo "$_greyp${(j:/:)cwd//\//$w/$_greyp}$w"
+}
+
+function minimal_lprompt {
+  local _venv=""
+  if [ -n "$VIRTUAL_ENV" ]; then
+    _venv="$(basename $VIRTUAL_ENV)"
+    _venv="${_venv%%.*} "
+  fi
+  local user_status="%{[%(1j.4.0);3%(0?.2.1)m%}%(!.#.$MINIMAL_USER_CHAR)"
+  local viins="$MINIMAL_INSERT_CHAR"
+  [ "$KEYMAP" = 'vicmd' ] && viins="$MINIMAL_NORMAL_CHAR"
+
+  echo "$_venv$user_status%{\e[0m%} $viins"
+}
+
+function minimal_ps2 {
+  local _venv=""
+  if [ -n "$VIRTUAL_ENV" ]; then
+    _venv="$(basename $VIRTUAL_ENV)"
+    _venv="${_venv%%.*} "
+  fi
+  local viins="$MINIMAL_INSERT_CHAR"
+  local offset="$((${#_venv} + 2))"
+  [ "$KEYMAP" = 'vicmd' ] && viins="$MINIMAL_NORMAL_CHAR"
+
+  printf " %.0s" {1..$offset}
+  echo "$viins"
+}
+
+if [ "$MINIMAL_PROMPT" = "yes" ]; then
+    # prompt redraw on vimode change
+    function zle-line-init zle-keymap-select {
+        zle reset-prompt
+    }
+
+    zle -N zle-line-init
+    zle -N zle-keymap-select
+
+    # prompts are set if not already set
+    PROMPT='$(minimal_lprompt) '
+    PS2='$(minimal_ps2) '
+    if [ "$MINIMAL_RPROMPT" = "yes" ]; then
+        RPROMPT='$(minimal_path)$(minimal_git)'
+    fi
+fi
+
+
+# MAGIC ENTER
+# magic enter: if no command is written,
+# hitting enter will display some info
+function minimal_magic_enter {
+  if [ -z "$BUFFER" ]; then
+    local w="[0m"
+
+    local user_host_pwd="$_grey%n$w@$_grey%m$w:$_grey%~$w"
+    user_host_pwd="${${(%)user_host_pwd}//\//$w/$_grey}"
+
+    local v_files="$(ls -1 | wc -l)"
+    local h_files="$(ls -1a | grep '^\.[^\.]' | wc -l)"
+
+    local job_n="$(jobs | wc -l)"
+
+    local iline="[$user_host_pwd] [$_grey$v_files$w ($_grey$h_files$w)]"
+    [ "$job_n" -gt 0 ] && iline="$iline [$_grey$job_n$w&]"
+
+    printf "$iline\n"
+
+    # listing
+    local output="$(ls -C --color="always")"
+
+    # git status
+    local git_status="$(git -c color.status=always status -s 2> /dev/null)"
+    if [ -n "$git_status" ]; then
+      output="$output\n$git_status"
+    fi
+
+    local output_len="$(echo "$output" | wc -l)"
+    if [ -n "$output" ]; then
+      if [ "$output_len" -gt "$((LINES - 2))" ]; then
+        printf "$output\n" | "$PAGER" -R
+      else
+        printf "$output\n"
+      fi
+    fi
+    zle redisplay
+  else
+    zle accept-line
+  fi
+}
+
+if [ "$MINIMAL_MAGIC_ENTER" = "yes" ]; then
+    zle -N minimal_magic_enter
+    bindkey "^M" minimal_magic_enter
+fi
+
