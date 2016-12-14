256f941ae1cf6b3e8f2ad2261cfecd0f3a74d5f8
subnixr <subnixr@gmx.com>
2016-12-14 21:36:16 +0100

    major refactoring

--- README.md
+++ README.md
@@ -17,42 +17,17 @@ antigen theme subnixr/minimal
 
 Otherwise, you can always clone the repo and source `minimal.zsh`.
 
-# Configuration
-This theme can be customized before sourcing through variables:
-
-- `MINIMAL_PROMPT`: enables prompt customization (default: active)
-- `MINIMAL_RPROMPT`: enables right prompt (default: active)
-- `MINIMAL_MAGIC_ENTER`: enables magic enter (default: active)
-- `MINIMAL_USER_CHAR`: overrides normal user character (default: `λ`)
-- `MINIMAL_INSERT_CHAR`: overrides insert mode character (default: `›`)
-- `MINIMAL_NORMAL_CHAR`: overrrides normal mode character (default: `·`)
-- `MINIMAL_OK_COLOR`: overrides color for successful last command and git clean 
-(default: `2`)
-- `MINIMAL_PWD_LEN`: overrides number of working directory segments shown 
-(default: `2`)
-- `MINIMAL_PWD_CHAR_LEN`: overrides maximum length of pwd's segments before it 
-is elided (default: `10`, minimum: `4`)
-
-To disable a feature, just set it to something other than `yes` before  sourcing.
-
-Example:
-```
-MINIMAL_MAGIC_ENTER="I Don't want this"
-source /path/to/minimal.zsh
-```
-
 # Description
 A prompt with all features:
 ```
-asd λ ›                                                 Workspace/sample master
+minimal_env λ ›                                     Workspace/sample minimal_vcs
 ```
 On the left:
 
-- `asd` is the `basename` of python's virtualenv currently in use. It won't be 
-shown if not in use.
+- `minimal_env` output (see *functions* section)
 - `λ` is shown if you are a normal user. When root, a classic `#` will be shown
 instead.
-- `λ` will be `MINIMAL_OK_COLOR` if the last command exited successfully, 
+- `λ` will be `$MINIMAL_OK_COLOR` if the last command exited successfully, 
 otherwise will be red.
 - `λ` will be underlined if you have jobs in background.
 - `›` will be show if you are in insert (default) mode. If using vimode, `·` 
@@ -60,11 +35,10 @@ is shown when in normal mode.
 
 On the right:
 
-- The last `MINIMAL_PWD_LEN` segments of `pwd` are shown. If a segment is 
-longer than `MINIMAL_PWD_CHAR_LEN` chars, it will be elided. 
-If you are near the root (eg: `/usr/bin`) the first slash will be shown.
-- Git's current branch. It will be shown only if inside a git repo. It will be 
-`MINIMAL_OK_COLOR` if clean, otherwise red.
+- The last `$MINIMAL_PWD_LEN` segments of `pwd` are shown. If a segment is 
+longer than `$MINIMAL_PWD_CHAR_LEN` chars, it will be elided. If you are near 
+the root (eg: `/usr/bin`) the first slash will be shown.
+- `minimal_vcs` output (see *functions* section)
 
 # Magic Enter
 You may miss some info from your prompt: magic enter to the rescue!
@@ -79,10 +53,73 @@ If your buffer is empty, pressing enter will print some useful informations:
 - Number of visible files, number of total files.
 - Number of background jobs (if any).
 - Exit code of last command (if exited with non-zero)
-- Colored `ls` output, wrapped in `$PAGER` if output is longer than your view.
-- Colored, brief, `git status` output, only if in a git repo
+- `minimal_magic_output` output (see *functions* section)
 
-#  I HATE UTF-8!
+# Configuration
+Declarations must be done **before** sourcing `minimal.zsh`, except for 
+*parameters*.
+
+## Features
+These variables are read when sourcing.
+
+- `MINIMAL_PROMPT`: left prompt (default: active).
+- `MINIMAL_RPROMPT`: right prompt (default: active, doesn't apply if 
+`MINIMAL_PROMPT` isn't enabled).
+- `MINIMAL_MAGIC_ENTER`: magic enter (default: active).
+
+To disable a feature, just set it to something other than `yes` before sourcing.
+
+Example:
+```
+MINIMAL_MAGIC_ENTER="I Don't want this"
+source /path/to/minimal.zsh
+```
+
+## Parameters
+Changes to these variables take immediate effect.
+
+- `MINIMAL_USER_CHAR`: normal user character (default: `λ`).
+- `MINIMAL_INSERT_CHAR`: insert mode character (default: `›`).
+- `MINIMAL_NORMAL_CHAR`: normal mode character (default: `·`).
+- `MINIMAL_OK_COLOR`: color for successful last command and git clean 
+(default: `2`).
+- `MINIMAL_PWD_LEN`: number of working directory segments shown (default: `2`).
+- `MINIMAL_PWD_CHAR_LEN`: maximum length of pwd's segments before it is elided
+(default: `10`, minimum: `4`).
+
+## Functions
+These are used by *minimal* to display useful informations.
+
+`minimal_env`: 
+
+Displays python's virtualenv name if activated.
+
+`minimal_vcs`:
+
+Displays current branch name, red if dirty, `$MINIMAL_OK_COLOR` if clean.
+
+`minimal_magic_output`:
+
+Displays colored `ls` output followed by a short, colored `git status`.
+
+The output of this functions is piped through `$PAGER` if output is longer 
+than `$LINES - 2`. This feature (piping) is not controlled by this function.
+
+To override a function, simply declare it before sourcing. Keep in mind that 
+override is total.
+
+Example:
+```
+# I want to drop some sick ascii art when hitting enter
+minimal_magic_output() {
+  figlet -f slant "COOLEST KID EVER!!"
+}
+source /path/to/minimal.zsh
+```
+
+# FAQ
+
+## I HATE UTF-8!
 No problem:
 ```
 MINIMAL_USER_CHAR="$"
@@ -91,3 +128,44 @@ MINIMAL_NORMAL_CHAR="-"
 source /path/to/minimal.zsh
 ```
 
+## I don't like that magic enter output is wrapped into `$PAGER`
+You have two choices:
+
+1. `unset $PAGER` (I don't know how much is *encouraged* this practice).
+2. Fork and roll your own, with blackjack and hookers.
+
+I personally find that is useless to have a long output if I can't scroll it.
+Sure, terminal emulators can scroll, but what happens when you can't use your 
+favorite one and you're stuck in a tty? `$PAGER`, that's the answer.
+
+## How do I change that info bar when using magic enter?
+For now, you can't. It's a feature of the theme.
+
+## Why `MINIMAL_OK_COLOR` but red is hardcoded?
+By desing, this theme has only one accent color (`MINIMAL_OK_COLOR`).
+Red is just a simple way to attract attention to something that needs it (error 
+code, branch dirty, etc.)
+
+## Do I need to set vi keybinding to use this theme?
+Absolutely no, and you will always see `$MINIMAL_INSERT_CHAR`.
+
+## I don't like feature X
+Change theme or see Bender's quote above.
+
+## Can you please add/change feature X?
+If you open an issue, I'll take it in consideration, but more often than not 
+you can achieve the same result with overrides.
+
+If you think your override is a better default than the current one, don't be 
+shy, but provide a clear and useful explanation about why it's better.
+
+Trivial changes (ex: "default normal user char to `$`") are not taken in 
+consideration.
+
+Also keep in mind that this theme wants to be fast and stays out the way of the 
+user. For example, checking the weather in the magic enter can be slow and 
+depends on connectivity; It will never be a default (you have overrides for 
+this kind of things).
+
+## Seriously, *magic enter*?
+I couldn't find a better name for it. I you have it, please tell me.
--- minimal.zsh
+++ minimal.zsh
@@ -1,9 +1,9 @@
-# enable/disable switches
+# Switches
 MINIMAL_PROMPT="${MINIMAL_PROMPT:-yes}"
 MINIMAL_RPROMPT="${MINIMAL_RPROMPT:-yes}"
 MINIMAL_MAGIC_ENTER="${MINIMAL_MAGIC_ENTER:-yes}"
 
-# customization parameters
+# Parameters
 MINIMAL_OK_COLOR="${MINIMAL_OK_COLOR:-2}"
 MINIMAL_USER_CHAR="${MINIMAL_USER_CHAR:-λ}"
 MINIMAL_INSERT_CHAR="${MINIMAL_INSERT_CHAR:-›}"
@@ -11,80 +11,143 @@ MINIMAL_NORMAL_CHAR="${MINIMAL_NORMAL_CHAR:-·}"
 MINIMAL_PWD_LEN="${MINIMAL_PWD_LEN:-2}"
 MINIMAL_PWD_CHAR_LEN="${MINIMAL_PWD_CHAR_LEN:-10}"
 
-# necessary
+# check if function exists
+function _isfn {
+    type -w $1 | grep -wq function
+}
+
+# Extensions
+if ! _isfn minimal_magic_output; then
+    function minimal_magic_output {
+        if [ "$(uname)" = "Darwin" ] && ! ls --version &> /dev/null; then
+            ls -C -G
+        else
+            ls -C --color="always" -w $COLUMNS
+        fi
+
+        git -c color.status=always status -sb 2> /dev/null
+    }
+fi
+
+if ! _isfn minimal_vcs; then
+    function minimal_vcs {
+        # git
+        local statc="%{\e[0;3${MINIMAL_OK_COLOR}m%}" # assumes is clean
+        local bname="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
+
+        if [ -n "$bname" ]; then
+            [ -n "$(git status --porcelain 2> /dev/null)" ] &&\
+                statc="%{\e[0;31m%}"
+            echo -n " $statc$bname%{\e[0m%}"
+        fi
+    }
+fi
+
+if ! _isfn minimal_env; then
+    function minimal_env {
+        # python virtual env
+        if [ -n "$VIRTUAL_ENV" ]; then
+            _venv="$(basename $VIRTUAL_ENV)"
+            echo -n "${_venv%%.*} "
+        fi
+    }
+fi
+
+# Setup
 autoload -U colors && colors
 setopt prompt_subst
 
 _grey="\e[38;5;244m"
 _greyp="%{$_grey%}"
 
-# os detection, linux as default
-function minimal_ls {
-    ls -C --color="always" -w $COLUMNS
-}
-
-if [[ "$(uname)" = "Darwin" ]] && ! ls --version &> /dev/null; then
-    function minimal_ls {
-        ls -C -G
-    }
-fi
-
-function minimal_git {
-  local statc="%{\e[0;3${MINIMAL_OK_COLOR}m%}" # assumes is clean
-  local bname="$(git rev-parse --abbrev-ref HEAD 2> /dev/null)"
-
-  if [ -n "$bname" ]; then
-    [ -n "$(git status --porcelain 2> /dev/null)" ] && statc="%{\e[0;31m%}"
-    echo " $statc$bname%{\e[0m%}"
-  fi
-}
-
-function minimal_path {
-  local w="%{\e[0m%}"
-  local cwd="%${MINIMAL_PWD_LEN}~"
-  local pi=""
-  local len="$MINIMAL_PWD_CHAR_LEN"
-  [ "$len" -lt 4 ] && len=4
-  local hlen=$((len / 2 - 1))
-  cwd="${(%)cwd}"
-  cwd=("${(@s:/:)cwd}")
-
-  for i in {1..${#cwd}}; do
-    pi="$cwd[$i]"
-    [ "${#pi}" -gt "$len" ] && cwd[$i]="${pi:0:$hlen}$w..$_greyp${pi: -$hlen}"
-  done
-
-  echo "$_greyp${(j:/:)cwd//\//$w/$_greyp}$w"
-}
-
+# Left Prompt
 function minimal_lprompt {
-  local _venv=""
-  if [ -n "$VIRTUAL_ENV" ]; then
-    _venv="$(basename $VIRTUAL_ENV)"
-    _venv="${_venv%%.*} "
-  fi
-  local user_status="%{\e[%(1j.4.0);3%(0?.$MINIMAL_OK_COLOR.1)m%}\
+    local user_status="%{\e[%(1j.4.0);3%(0?.$MINIMAL_OK_COLOR.1)m%}\
 %(!.#.$MINIMAL_USER_CHAR)"
-  local viins="$MINIMAL_INSERT_CHAR"
-  [ "$KEYMAP" = 'vicmd' ] && viins="$MINIMAL_NORMAL_CHAR"
+    local kmstatus="$MINIMAL_INSERT_CHAR"
+    [ "$KEYMAP" = 'vicmd' ] && kmstatus="$MINIMAL_NORMAL_CHAR"
 
-  echo "$_venv$user_status%{\e[0m%} $viins"
+    echo -n "$user_status%{\e[0m%} $kmstatus"
 }
 
 function minimal_ps2 {
-  local _venv=""
-  if [ -n "$VIRTUAL_ENV" ]; then
-    _venv="$(basename $VIRTUAL_ENV)"
-    _venv="${_venv%%.*} "
-  fi
-  local viins="$MINIMAL_INSERT_CHAR"
-  local offset="$((${#_venv} + 2))"
-  [ "$KEYMAP" = 'vicmd' ] && viins="$MINIMAL_NORMAL_CHAR"
+    local kmstatus="$MINIMAL_INSERT_CHAR"
+    local offset="$((${#_venv} + 2))"
+    [ "$KEYMAP" = 'vicmd' ] && kmstatus="$MINIMAL_NORMAL_CHAR"
 
-  printf " %.0s" {1..$offset}
-  echo "$viins"
+    printf " %.0s" {1..$offset}
+    echo -n "$kmstatus"
 }
 
+# Right Prompt
+function minimal_path {
+    local w="%{\e[0m%}"
+    local cwd="%${MINIMAL_PWD_LEN}~"
+    local pi=""
+    local len="$MINIMAL_PWD_CHAR_LEN"
+    [ "$len" -lt 4 ] && len=4
+    local hlen=$((len / 2 - 1))
+    cwd="${(%)cwd}"
+    cwd=("${(@s:/:)cwd}")
+
+    for i in {1..${#cwd}}; do
+        pi="$cwd[$i]"
+        [ "${#pi}" -gt "$len" ] && cwd[$i]="${pi:0:$hlen}$w..$_greyp${pi: -$hlen}"
+    done
+
+    echo -n "$_greyp${(j:/:)cwd//\//$w/$_greyp}$w"
+}
+
+# Magic Enter
+function minimal_infoline {
+        local last_err="$1"
+        local w="\e[0m"
+        local rn="\e[0;31m"
+        local rb="\e[1;31m"
+
+        local user_host_pwd="$_grey%n$w@$_grey%m$w:$_grey%~$w"
+        user_host_pwd="${${(%)user_host_pwd}//\//$w/$_grey}"
+
+        local v_files="$(ls -1 | wc -l)"
+        local h_files="$(ls -1A | wc -l)"
+
+        local job_n="$(jobs | wc -l)"
+
+        local iline="[$user_host_pwd] [$_grey$v_files$w ($_grey$h_files$w)]"
+        [ "$job_n" -gt 0 ] && iline="$iline [$_grey$job_n$w&]"
+
+        if [ "$last_err" != "0" ]; then
+                iline="$iline \e[1;31m[\e[0;31m$last_err\e[1;31m]$w"
+        fi
+
+        echo "$iline"
+}
+
+function minimal_wrap_output {
+    local output="$1"
+    local output_len="$(echo "$output" | wc -l)"
+    if [ -n "$output" ]; then
+        if [ "$output_len" -gt "$((LINES - 2))" -a -n "$PAGER" ]; then
+            printf "$output\n" | "$PAGER" -R
+        else
+            printf "$output\n"
+        fi
+    fi
+}
+
+function minimal_magic_enter {
+    local last_err="$?" # I need to capture this ASAP
+
+    if [ -z "$BUFFER" ]; then
+        minimal_infoline $last_err
+        minimal_wrap_output "$(minimal_magic_output)"
+        zle redisplay
+    else
+        zle accept-line
+    fi
+}
+
+# Apply Switches
 if [ "$MINIMAL_PROMPT" = "yes" ]; then
     # prompt redraw on vimode change
     function zle-line-init zle-keymap-select {
@@ -94,68 +157,12 @@ if [ "$MINIMAL_PROMPT" = "yes" ]; then
     zle -N zle-line-init
     zle -N zle-keymap-select
 
-    # prompts are set if not already set
-    PROMPT='$(minimal_lprompt) '
+    PROMPT='$(minimal_env)$(minimal_lprompt) '
     PS2='$(minimal_ps2) '
-    if [ "$MINIMAL_RPROMPT" = "yes" ]; then
-        RPROMPT='$(minimal_path)$(minimal_git)'
-    fi
+    [ "$MINIMAL_RPROMPT" = "yes" ] && RPROMPT='$(minimal_path)$(minimal_vcs)'
 fi
-
-
-# MAGIC ENTER
-# magic enter: if no command is written,
-# hitting enter will display some info
-function minimal_magic_enter {
-  local last_err="$?"
-
-  if [ -z "$BUFFER" ]; then
-    local w="\e[0m"
-    local rn="\e[0;31m"
-    local rb="\e[1;31m"
-
-    local user_host_pwd="$_grey%n$w@$_grey%m$w:$_grey%~$w"
-    user_host_pwd="${${(%)user_host_pwd}//\//$w/$_grey}"
-
-    local v_files="$(ls -1 | wc -l)"
-    local h_files="$(ls -1A | wc -l)"
-
-    local job_n="$(jobs | wc -l)"
-
-    local iline="[$user_host_pwd] [$_grey$v_files$w ($_grey$h_files$w)]"
-    [ "$job_n" -gt 0 ] && iline="$iline [$_grey$job_n$w&]"
-
-    if [ "$last_err" != "0" ]; then
-        iline="$iline \e[1;31m[\e[0;31m$last_err\e[1;31m]$w"
-    fi
-
-    printf "$iline\n"
-
-    # listing
-    local output="$(minimal_ls)"
-
-    # git status
-    local git_status="$(git -c color.status=always status -s 2> /dev/null)"
-    if [ -n "$git_status" ]; then
-      output="$output\n$git_status"
-    fi
-
-    local output_len="$(echo "$output" | wc -l)"
-    if [ -n "$output" ]; then
-      if [ "$output_len" -gt "$((LINES - 2))" ]; then
-        printf "$output\n" | "$PAGER" -R
-      else
-        printf "$output\n"
-      fi
-    fi
-    zle redisplay
-  else
-    zle accept-line
-  fi
-}
 
 if [ "$MINIMAL_MAGIC_ENTER" = "yes" ]; then
     zle -N minimal_magic_enter
     bindkey "^M" minimal_magic_enter
 fi
-
